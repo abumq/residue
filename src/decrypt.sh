d32682f0005a6ba257e07638e2c3082869eea3a0
mkhan <mkhan@allhomes.com.au>
2017-07-27 15:10:55 +1000

    use var ripe

--- src/decrypt.sh
+++ src/decrypt.sh
@@ -11,21 +11,21 @@ if [ "$RESIDUE_SRC_KEY" = "" ];then
     exit;
 fi
 
-result=$(cat $file | $RIPE -d --aes --key $RESIDUE_SRC_KEY --base64 | ripe -e --hex)
+result=$(cat $file | $RIPE -d --aes --key $RESIDUE_SRC_KEY --base64 | $RIPE -e --hex)
 
 if [ -f $newfile ]; then
-   orig=`cat $newfile | ripe -e --hex`
+   orig=`cat $newfile | $RIPE -e --hex`
    if [ "$orig" = "$result" ];then
        echo "Ignoring for same file"
    else
        echo "$orig" > "$newfile.bk"
        echo "$result" > $newfile.hex
-       cat $newfile.hex | ripe -d --hex > $newfile
+       cat $newfile.hex | $RIPE -d --hex > $newfile
        rm $newfile.hex
    fi
 else
    echo "$result" > $newfile.hex
-   cat $newfile.hex | ripe -d --hex > $newfile
+   cat $newfile.hex | $RIPE -d --hex > $newfile
    rm $newfile.hex
 fi
 
