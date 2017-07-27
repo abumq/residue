14aaee2767a805d7ef472ffd60e99e0bc7073bcb
mkhan <mkhan@allhomes.com.au>
2017-07-27 13:36:18 +1000

    update

--- src/decrypt.sh
+++ src/decrypt.sh
@@ -1,4 +1,6 @@
 file=$1
+newfile="${file%????}"
+
 if [ "$RIPE" = "" ];then
     RIPE="ripe"
 fi
@@ -9,4 +11,17 @@ if [ "$RESIDUE_SRC_KEY" = "" ];then
     exit;
 fi
 
-cat $file | $RIPE -d --aes --key $RESIDUE_SRC_KEY --base64 > "${file%????}"
+result=$(cat $file | $RIPE -d --aes --key $RESIDUE_SRC_KEY --base64)
+
+if [ -f $newfile ]; then
+   orig=`cat $newfile`
+   if [ "$orig" = "$result" ];then
+       echo "Ignoring for same file"
+   else
+       echo "$orig" > "$newfile.bk"
+       echo "$result" > $newfile
+   fi
+else
+   echo "$result" > $newfile
+fi
+
