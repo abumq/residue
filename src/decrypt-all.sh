c1dc494f426125e9723ce4dfc6c4848e607b8a10
mkhan <mkhan@allhomes.com.au>
2017-07-27 12:58:21 +1000

    fix

--- src/decrypt-all.sh
+++ src/decrypt-all.sh
@@ -2,5 +2,5 @@ if [ "$1" == "" ];then
     echo "No key provided!"
     exit;
 fi
-find . -name "*.enc.cc" -exec sh decrypt.sh {} $1 \;
-find . -name "*.enc.h" -exec sh decrypt.sh {} $1 \;
+find . -name "*.enc" -exec sh decrypt.sh {} $1 \;
+find . -name "*.enc" -exec sh decrypt.sh {} $1 \;
