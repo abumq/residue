6ebb622b882be62eaec5624d195466d7e31701c6
Majid Khan <mkhan3189@gmail.com>
2017-08-01 22:09:22 +1000

    ver

--- src/decrypt-all.sh
+++ src/decrypt-all.sh
@@ -1,8 +1,11 @@
 if [ "$RIPE" = "" ];then
     export RIPE="ripe"
 fi
+
 echo "RIPE = $RIPE"
 echo "LD_LIBRARY_PATH = $LD_LIBRARY_PATH"
+
+$RIPE --version
 
 find . -name "*.enc" -exec sh decrypt.sh {} \;
 find . -name "*.enc" -exec sh decrypt.sh {} \;
