392bc59353ea431fb5b7b5cf6655c81e21d82293
mkhan <mkhan@allhomes.com.au>
2017-08-04 09:25:59 +1000

    update

--- src/decrypt.sh
+++ src/decrypt.sh
@@ -12,10 +12,12 @@ if [ -f $newfile ]; then
    orig=`cat $newfile | $RIPE -e --hex`
    if [ "$orig" != "$result" ];then
        echo "Updating $file... (orig file backedup)"
-       echo "$orig" > "$newfile.bk"
+       echo "$orig" > "$newfile.bk.hex"
        echo "$result" > $newfile.hex
        cat $newfile.hex | $RIPE -d --hex > $newfile
+       cat $newfile.bk.hex | $RIPE -d --hex > $newfile.bk
        rm $newfile.hex
+       rm $newfile.bk.hex
    fi
 else
    echo "Updating $file..."
