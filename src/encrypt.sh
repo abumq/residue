b31454295c93502018b7004e53d5806a2189fcad
mkhan <mkhan@allhomes.com.au>
2017-08-04 09:18:48 +1000

    enc update pwd

--- src/encrypt.sh
+++ src/encrypt.sh
@@ -1,5 +1,4 @@
 file=$1
-key=$2
 
 if [ "$RESIDUE_SRC_KEY" = "" ];then
     echo "RESIDUE_SRC_KEY not set"
@@ -9,7 +8,7 @@ fi
 FILE_CHECKSUM=`shasum $file`
 CURR_CHECKSUM=`cat $file.chk`
 
-if [ "$FILE_SHA" != "$CURR_CHECKSUM" ];then
+if [ "$FILE_CHECKSUM" != "$CURR_CHECKSUM" ];then
     cat $file | $RIPE -e --aes --key $RESIDUE_SRC_KEY > $file.enc
     echo "$FILE_CHECKSUM" > $file.chk
 fi
