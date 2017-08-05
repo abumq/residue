3d6d6c03c28148df7283494478f7e61a782f5c57
Majid Khan <mkhan3189@gmail.com>
2017-08-05 21:49:28 +1000

    config validator compile

--- tools/license-manager/main.cc
+++ tools/license-manager/main.cc
@@ -11,11 +11,12 @@
 #include <iostream>
 #include <string>
 #include "src/LicenseManager.h"
+#include "src/License.h"
 
 using namespace residue;
 
 void displayUsage() {
-    std::cout << "residue-license-manager [--validate <file> <signature>] [--issue <licensee> <licensee_signature> <validation_period> <issuing_authority>]" << std::endl;
+    std::cout << "residue-license-manager [--validate <file> --signature <signature>] [--issue --licensee <licensee> --signature <licensee_signature> --period <validation_period> --authority <issuing_authority>]" << std::endl;
 }
 
 void displayVersion() {
@@ -37,6 +38,10 @@ int main(int argc, char* argv[])
 
     std::string licenseFile;
     std::string signature;
+    std::string licensee;
+    std::string authority = "default";
+    unsigned int period;
+    bool issue = false;
 
     for (int i = 0; i < argc; i++) {
         std::string arg(argv[i]);
@@ -45,6 +50,13 @@ int main(int argc, char* argv[])
         } else if (arg == "--signature" && i < argc) {
             signature = argv[++i];
         } else if (arg == "--issue" && i < argc) {
+            issue = true;
+        } else if (arg == "--licensee" && i < argc) {
+            licensee = argv[++i];
+        } else if (arg == "--period" && i < argc) {
+            period = static_cast<unsigned int>(atoi(argv[++i]));
+        } else if (arg == "--authority" && i < argc) {
+            authority = argv[++i];
         }
     }
 
@@ -59,13 +71,23 @@ int main(int argc, char* argv[])
                                               (std::istreambuf_iterator<char>()));
             stream.close();
             residue::License license;
-            if (!licenseManager.validate(licenseKey, license, true, signature)) {
+            if (!licenseManager.validate(licenseKey, &license, true, signature)) {
                 std::cout << "License is not valid";
             } else {
                 std::cout << "Licensed to " << license.licensee() << std::endl;
                 std::cout << "Subscription is active until " << license.formattedExpiry() << std::endl << std::endl;
             }
         }
+    } else if (issue) {
+#ifdef LICENSE_MANAGER_CAN_ISSUE_LICENSE
+        licenseManager.changeIssuingAuthority(authority);
+        License license = licenseManager.generateNew(licensee, period, signature);
+        std::cout << license.toString() << std::endl;
+        std::cout << "Licensed to " << license.licensee() << std::endl;
+        std::cout << "Subscription is active until " << license.formattedExpiry() << std::endl << std::endl;
+#else
+        std::cerr << "ERROR: Access denied. You cannot issue new licenses." << std::endl;
+#endif // LICENSE_MANAGER_CAN_ISSUE_LICENSE
     }
     return 0;
 }
