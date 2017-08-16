19f5e0df32f42961bafc80dcb5ac6831d4f00f34
Majid Khan <mkhan3189@gmail.com>
2017-08-17 09:46:34 +1000

    some more performance updates and code stanards

--- test/url-test.h
+++ test/url-test.h
@@ -14,6 +14,9 @@
 using namespace residue;
 
 static TestData<std::string, std::string, std::string, std::string, std::string, std::string> URLTestData = {
+    TestCase("https://192.168.1.100:3322/p1?q=1", "https", "192.168.1.100", "3322", "/p1", "q=1"),
+    TestCase("http://192.168.1.19:3000/p2?q2=2", "http", "192.168.1.19", "3000", "/p2", "q2=2"),
+    TestCase("HTTP://LOCALHOST:3000/PATH?QUERY1=1&QUERY2=2", "HTTP", "LOCALHOST", "3000", "/PATH", "QUERY1=1&QUERY2=2"),
     TestCase("http://localhost:3000/path?query1=1&query2=2", "http", "localhost", "3000", "/path", "query1=1&query2=2"),
     TestCase("http://localhost", "http", "localhost", "80", "", ""),
     TestCase("http://localhost:3000", "http", "localhost", "3000", "", ""),
@@ -27,18 +30,56 @@ static TestData<std::string, std::string, std::string, std::string, std::string,
 
 };
 
+void testUrl(const Url& url, const std::tuple<std::string, std::string, std::string, std::string, std::string, std::string>& item)
+{
+    ASSERT_EQ(url.protocol(), PARAM(1));
+    ASSERT_EQ(url.host(), PARAM(2));
+    ASSERT_EQ(url.port(), PARAM(3));
+    ASSERT_EQ(url.path(), PARAM(4));
+    ASSERT_EQ(url.query(), PARAM(5));
+    ASSERT_TRUE(url.isValid());
+}
+
 TEST(UrlTest, TestUrls)
 {
     for (const auto& item : URLTestData) {
         LOG(INFO) << "Testing " << PARAM(0);
         Url url(PARAM(0));
-        ASSERT_EQ(PARAM(1), url.protocol());
-        ASSERT_EQ(PARAM(2), url.host());
-        ASSERT_EQ(PARAM(3), url.port());
-        ASSERT_EQ(PARAM(4), url.path());
-        ASSERT_EQ(PARAM(5), url.query());
-        ASSERT_TRUE(url.isValid());
+        testUrl(url, item);
     }
+}
+
+TEST(UrlTest, TestUrlsCopy)
+{
+    for (const auto& item : URLTestData) {
+        LOG(INFO) << "Testing " << PARAM(0);
+        Url url(PARAM(0));
+        Url url2(url);
+        testUrl(url2, item);
+    }
+}
+
+TEST(UrlTest, TestUrlsAssignment)
+{
+    const auto& item1 = URLTestData.at(0);
+    const auto& item2 = URLTestData.at(1);
+
+    Url url1(std::get<0>(item1));
+    Url url2(std::get<0>(item2));
+    Url url3(std::get<0>(item1));
+
+    testUrl(url1, item1);
+    testUrl(url2, item2);
+
+    url1 = url2;
+
+    testUrl(url2, item2);
+    testUrl(url1, item2);
+
+    url2 = url3;
+
+    testUrl(url2, item1);
+    testUrl(url1, item2);
 }
 
 
