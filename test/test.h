64e10311fc307520853efa947c039b7d57cdcfca
mkhan <mkhan@allhomes.com.au>
2017-08-30 14:13:40 +1000

    use testx

--- test/test.h
+++ test/test.h
@@ -8,20 +8,12 @@
 #ifndef TEST_H
 #define TEST_H
 
-#include <vector>
-#include <tuple>
-
+#include "testx.h"
 #include <easylogging++.h>
 #include <gtest/gtest.h>
 
-template <typename... T>
-using TestData = const std::vector<std::tuple<T...>>;
-
-template <typename... T>
-std::tuple<T...> TestCase(T... f) {
-    return std::make_tuple(f...);
-}
-
 #define PARAM(v) std::get<v>(item)
+
+using namespace muflihun::testx;
 
 #endif // TEST_H
