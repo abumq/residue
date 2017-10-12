3f73566a5c2b481a75efdfd2942c686fab271b56
mkhan <mkhan@allhomes.com.au>
2017-10-13 10:28:32 +1100

    py with multi-threading

--- include/Python.h
+++ include/Python.h
@@ -5,11 +5,11 @@
 //  Copyright © 2017 Muflihun Labs
 //
 
-#ifndef Python_h
-#define Python_h
+#ifndef ResiduePython_h
+#define ResiduePython_h
 
 #ifdef RESIDUE_HAS_EXTENSIONS
 #   include <python2.7/Python.h>
 #endif
 
-#endif /* Python_h */
+#endif /* ResiduePython_h */
