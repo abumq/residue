a2422f9c0f69b899fd4a286c4ede94cad91cc81b
mkhan <mkhan@allhomes.com.au>
2017-10-12 11:13:14 +1100

    update to use common header for python

---
+++ include/Python.h
@@ -0,0 +1,15 @@
+//
+//  python.h
+//  Residue
+//
+//  Copyright © 2017 Muflihun Labs
+//
+
+#ifndef Python_h
+#define Python_h
+
+#ifdef RESIDUE_HAS_EXTENSIONS
+#   include <python2.7/Python.h>
+#endif
+
+#endif /* Python_h */
