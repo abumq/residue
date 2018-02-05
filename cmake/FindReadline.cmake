5228ae441aac2004cb6d19928c611ef0ad72fdae
mkhan <mkhan@allhomes.com.au>
2018-02-05 16:35:04 +1100

    depend on ncurses

--- cmake/FindReadline.cmake
+++ cmake/FindReadline.cmake
@@ -1,49 +1,80 @@
-# Code copied from google/ohmu@github
-#
-# - Try to find readline include dirs and libraries
-#
-# Usage of this module as follows:
-#
-#     find_package(Readline)
-#
-# Variables used by this module, they can change the default behaviour and need
-# to be set before calling find_package:
-#
-#  Readline_ROOT_DIR         Set this variable to the root installation of
-#                            readline if the module has problems finding the
-#                            proper installation path.
-#
-# Variables defined by this module:
-#
-#  READLINE_FOUND            System has readline, include and lib dirs found
-#  Readline_INCLUDE_DIR      The readline include directories.
-#  Readline_LIBRARY          The readline library.
+# - Find the readline library
+# This module defines
+#  READLINE_INCLUDE_DIR, path to readline/readline.h, etc.
+#  READLINE_LIBRARIES, the libraries required to use READLINE.
+#  READLINE_FOUND, If false, do not try to use READLINE.
+# also defined, but not for general use are
+# READLINE_readline_LIBRARY, where to find the READLINE library.
+# READLINE_ncurses_LIBRARY, where to find the ncurses library [might not be defined]
 
-find_path(Readline_ROOT_DIR
-    NAMES include/readline/readline.h
-)
+# Apple readline does not support readline hooks
+# So we look for another one by default
+IF(APPLE)
+  FIND_PATH(READLINE_INCLUDE_DIR NAMES readline/readline.h PATHS
+    /sw/include
+    /opt/local/include
+    /opt/include
+    /usr/local/include
+    /usr/include/
+    NO_DEFAULT_PATH
+    )
+ENDIF(APPLE)
+FIND_PATH(READLINE_INCLUDE_DIR NAMES readline/readline.h)
 
-find_path(Readline_INCLUDE_DIR
-    NAMES readline/readline.h
-    HINTS ${Readline_ROOT_DIR}/include
-)
 
-find_library(Readline_LIBRARY
-    NAMES libreadline.a readline
-    HINTS ${Readline_ROOT_DIR}/lib
-)
+# Apple readline does not support readline hooks
+# So we look for another one by default
+IF(APPLE)
+  FIND_LIBRARY(READLINE_readline_LIBRARY NAMES readline PATHS
+    /sw/lib
+    /opt/local/lib
+    /opt/lib
+    /usr/local/lib
+    /usr/lib
+    NO_DEFAULT_PATH
+    )
+ENDIF(APPLE)
+FIND_LIBRARY(READLINE_readline_LIBRARY NAMES readline)
 
-if(Readline_INCLUDE_DIR AND Readline_LIBRARY AND Ncurses_LIBRARY)
-  set(READLINE_FOUND TRUE)
-else(Readline_INCLUDE_DIR AND Readline_LIBRARY AND Ncurses_LIBRARY)
-  FIND_LIBRARY(Readline_LIBRARY NAMES libreadline.a readline)
-  include(FindPackageHandleStandardArgs)
-  FIND_PACKAGE_HANDLE_STANDARD_ARGS(Readline DEFAULT_MSG Readline_INCLUDE_DIR Readline_LIBRARY )
-  MARK_AS_ADVANCED(Readline_INCLUDE_DIR Readline_LIBRARY)
-endif(Readline_INCLUDE_DIR AND Readline_LIBRARY AND Ncurses_LIBRARY)
+# Sometimes readline really needs ncurses
+IF(APPLE)
+  FIND_LIBRARY(READLINE_ncurses_LIBRARY NAMES ncurses PATHS
+    /sw/lib
+    /opt/local/lib
+    /opt/lib
+    /usr/local/lib
+    /usr/lib
+    NO_DEFAULT_PATH
+    )
+ENDIF(APPLE)
+FIND_LIBRARY(READLINE_ncurses_LIBRARY NAMES ncurses)
 
-mark_as_advanced(
-    Readline_ROOT_DIR
-    Readline_INCLUDE_DIR
-    Readline_LIBRARY
-)
+MARK_AS_ADVANCED(
+  READLINE_INCLUDE_DIR
+  READLINE_readline_LIBRARY
+  READLINE_ncurses_LIBRARY
+  )
+
+SET( READLINE_FOUND "NO" )
+IF(READLINE_INCLUDE_DIR)
+  IF(READLINE_readline_LIBRARY)
+    SET( READLINE_FOUND "YES" )
+    SET( READLINE_LIBRARIES
+      ${READLINE_readline_LIBRARY}
+      )
+
+    # some readline libraries depend on ncurses
+    IF(READLINE_ncurses_LIBRARY)
+      SET(READLINE_LIBRARIES ${READLINE_LIBRARIES} ${READLINE_ncurses_LIBRARY})
+    ENDIF(READLINE_ncurses_LIBRARY)
+
+  ENDIF(READLINE_readline_LIBRARY)
+ENDIF(READLINE_INCLUDE_DIR)
+
+IF(READLINE_FOUND)
+  MESSAGE(STATUS "Found readline library")
+ELSE(READLINE_FOUND)
+  IF(READLINE_FIND_REQUIRED)
+    MESSAGE(FATAL_ERROR "Could not find readline -- please give some paths to CMake")
+  ENDIF(READLINE_FIND_REQUIRED)
+ENDIF(READLINE_FOUND)
