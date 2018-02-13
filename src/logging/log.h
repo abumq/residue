4971d2a41955657bd10afe0b749ca85d390f85c6
Majid <mkhan3189@gmail.com>
2018-02-13 23:27:09 +1100

    CHECKPOINT: disabling residue logger solves all the deadlocks and crashes

--- src/logging/log.h
+++ src/logging/log.h
@@ -31,14 +31,24 @@
 #else
 #   define RESIDUE_DEBUG_LOG 0
 #endif
-#define RLOG(LEVEL) CLOG(LEVEL, RESIDUE_LOGGER_ID)
-#define RLOG_IF(condition, LEVEL) CLOG_IF(condition, LEVEL, RESIDUE_LOGGER_ID)
-#define RVLOG(vLevel) CVLOG(vLevel, RESIDUE_LOGGER_ID)
-#define RVLOG_IF(cond, vLevel) CVLOG_IF(cond, vLevel, RESIDUE_LOGGER_ID)
-#define DRLOG(LEVEL) if (RESIDUE_DEBUG_LOG) DCLOG(LEVEL, RESIDUE_LOGGER_ID)
-#define DRVLOG(vLevel) if (RESIDUE_DEBUG_LOG) DCVLOG(vLevel, RESIDUE_LOGGER_ID)
-#define DRVLOG_IF(condition, vLevel) if (RESIDUE_DEBUG_LOG) DCVLOG_IF(condition, vLevel, RESIDUE_LOGGER_ID)
 
+#if 0
+    #define RLOG(LEVEL) CLOG(LEVEL, RESIDUE_LOGGER_ID)
+    #define RLOG_IF(condition, LEVEL) CLOG_IF(condition, LEVEL, RESIDUE_LOGGER_ID)
+    #define RVLOG(vLevel) CVLOG(vLevel, RESIDUE_LOGGER_ID)
+    #define RVLOG_IF(cond, vLevel) CVLOG_IF(cond, vLevel, RESIDUE_LOGGER_ID)
+    #define DRLOG(LEVEL) if (RESIDUE_DEBUG_LOG) DCLOG(LEVEL, RESIDUE_LOGGER_ID)
+    #define DRVLOG(vLevel) if (RESIDUE_DEBUG_LOG) DCVLOG(vLevel, RESIDUE_LOGGER_ID)
+    #define DRVLOG_IF(condition, vLevel) if (RESIDUE_DEBUG_LOG) DCVLOG_IF(condition, vLevel, RESIDUE_LOGGER_ID)
+#else
+    #define RLOG(LEVEL) el::base::NullWriter()
+    #define RLOG_IF(condition, LEVEL) el::base::NullWriter()
+    #define RVLOG(vLevel) el::base::NullWriter()
+    #define RVLOG_IF(cond, vLevel) el::base::NullWriter()
+    #define DRLOG(LEVEL) el::base::NullWriter()
+    #define DRVLOG(vLevel) el::base::NullWriter()
+    #define DRVLOG_IF(condition, vLevel) el::base::NullWriter()
+#endif
 #define RV_CRAZY 9
 #define RV_TRACE 8
 #define RV_DEBUG 7
