79372ebdaa0b7e6597cee7c2dd98107c3799644f
mkhan <mkhan@allhomes.com.au>
2018-03-08 13:20:02 +1100

    fix sample

--- tools/netcat-client/create_request-log.php
+++ tools/netcat-client/create_request-log.php
@@ -3,7 +3,6 @@
 // Usage: php create_request-log.php connection.json
 
 $connection = json_decode(file_get_contents($argv[1]));
-$encrypt = isset($argv[2]) && $argv[2] == "0" ? false : true;
 
 $req = array(
         "_t" => time(),
@@ -15,11 +14,5 @@ $req = array(
 	"app" => "Residue Netcat Sample",
 	"level" => 16,
 );
-if ($encrypt) {
-    $req = addslashes(json_encode($req));
-    echo shell_exec("echo $req | ripe -e --key $connection->key --client-id $connection->client_id");
-} else {
-    $req["client_id"] = $connection->client_id;
-    $req = addslashes(json_encode($req));
-    echo $req;
-}
+$req = addslashes(json_encode($req));
+echo shell_exec("echo $req | ripe -e --key $connection->key --client-id $connection->client_id");
